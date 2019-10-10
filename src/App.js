8f429290efb775291b4e6b8a5db356c05aed2cd5
Icaro Rezende <icaronr@gmail.com>
2019-10-10 14:12:28 -0300

    Add the TurmaPage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import SubjectPage from './components/SubjectPage';
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { Transition } from 'react-native-reanimated';
+import createAnimatedSwitchNavigator from 'react-navigation-animated-switch';
 
 const HomeScreen = ({ navigation }) => {
   return (
@@ -17,13 +18,24 @@ const SubjectScreen = () => {
   return <SubjectPage></SubjectPage>;
 };
 
-const AppNavigator = createStackNavigator(
+const AppNavigator = createAnimatedSwitchNavigator(
   {
     Home: HomeScreen,
     Subjects: SubjectScreen,
   },
   {
     initialRouteName: 'Home',
+    // The previous screen will slide to the bottom while the next screen will fade in
+    transition: (
+      <Transition.Together>
+        <Transition.Out
+          type="slide-bottom"
+          durationMs={400}
+          interpolation="easeIn"
+        />
+        <Transition.In type="fade" durationMs={500} />
+      </Transition.Together>
+    ),
   },
 );
 
